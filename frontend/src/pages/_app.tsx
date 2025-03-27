ab0dde98dff6b8ddf99d125ffa8598c93adbe235
de Laforcade Loïc <loic.de-laforcade@emgsa.ch>
2025-03-27 16:50:14 +0100

    :bug: Fix some navigations bug and re-design - Edit design - Fix Navigation - Add some translations

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app'
 import { appWithTranslation } from 'next-i18next'
 import '../styles/globals.css'
+import '@/styles/calendar.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
